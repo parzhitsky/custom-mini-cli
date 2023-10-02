2addae9ba099e85914981199932aaf53659ccfe1
Dmytro Parzhytskyi <parzhitsky@gmail.com>
2023-10-02 23:43:21 +0300

    Generalize array to an `Iterable`

--- src/object-from-entries.ts
+++ src/object-from-entries.ts
@@ -3,6 +3,6 @@ type ObjectFromEntries<Entry extends readonly [PropertyKey, unknown]> = {
   [K in Entry[0]]: Entry extends readonly [K, infer Value] ? Value : Entry[1]
 }
 
-export function objectFromEntries<Entry extends readonly [PropertyKey, unknown]>(entries: readonly Entry[]): ObjectFromEntries<Entry> {
+export function objectFromEntries<Entry extends readonly [PropertyKey, unknown]>(entries: Iterable<Entry>): ObjectFromEntries<Entry> {
   return Object.fromEntries(entries) as ObjectFromEntries<Entry>
 }
